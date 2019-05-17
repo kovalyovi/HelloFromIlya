999f6e02391b6e71c9d5c523dca6c0f846270346
kovalyovi <ilia.kovaliov@gmail.com>
2019-05-17 09:27:48 -0600

    another thing in testing

--- src/kovalyovi/Main.java
+++ src/kovalyovi/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Ilya, my name is Niles. ");
+        System.out.println("Another thing!");
     }
 }
